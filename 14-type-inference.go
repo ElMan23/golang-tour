d55c8ae4088e7ad00ab2c33d17efbf21441dfb3d
Elia Manara <e.manara90@gmail.com>
2021-02-19 13:53:06 +0000

    Other lectures

---
+++ 14-type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	w := 3.14
+	x := 0.8 + 0.2i
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("w is of type %T\n", w)
+	fmt.Printf("x is of type %T\n", x)
+}
